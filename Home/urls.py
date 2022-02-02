65da810dbf542b04ba065de64a7833b25e87dd2e
Willamy Domingos <willamy.wlp@gmail.com>
2022-02-02 11:54:29 -0300

    Correções no Template

--- Home/urls.py
+++ Home/urls.py
@@ -1,9 +1,13 @@
 
+from django.contrib import admin
 from django.urls import path
+
 from .views import home
 from SisRPF.views import logoutApp
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    
     path('', home, name="home"),
     path('logoutApp', logoutApp, name="logoutApp"),
 ]
