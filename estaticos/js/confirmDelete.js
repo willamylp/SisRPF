668958cbbcbf14e7ec8bf51b8fc3a842dcacc3f8
Willamy Domingos <willamy.wlp@gmail.com>
2022-03-31 21:28:39 -0300

    Ajsutando template tabela Grupo Familiar

---
+++ estaticos/js/confirmDelete.js
@@ -0,0 +1,45 @@
+function ConfirmarDelete(valor) {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-danger',
+            cancelButton: 'btn btn-success',
+        },
+        buttonsStyling: true
+    })
+
+    swalWithBootstrapButtons.fire({
+        title: 'Você tem certeza?',
+        text: "Você não poderá reverter isso!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: '✓ Deletar',
+        cancelButtonText: '✗ Cancelar',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            swalWithBootstrapButtons.fire(
+                'Deletado!',
+                'REGISTRO DELETADO COM SUCESSO!',
+                'success'
+            )
+            var url;
+            var urlAtual = window.location.href;
+            var urlAtual = urlAtual.split('/');
+            if (urlAtual.length > 6) {
+                url = '../../DeletarIntegrante/' + valor;
+            } else {
+                url = '../DeletarIntegrante/' + valor;
+            }
+            location.href = url;
+        } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+        ) {
+            swalWithBootstrapButtons.fire(
+                'Cancelado!',
+                'O REGISTRO <b>NÃO</b> FOI DELETADO!',
+                'info'
+            )
+        }
+    })
+}
